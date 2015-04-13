3a47369eee5582d2b6adc287a22104369c3ae25e
依田　剛 <takeshi@YodakeMBA.local>
2015-04-13 23:44:48 +0900

    no message

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,7 +3,7 @@
 
 # --- !Ups
 
-create table "tanbo" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"liceType" INTEGER NOT NULL,"phase" INTEGER NOT NULL,"doneDate" VARCHAR(254) NOT NULL,"latitude" DOUBLE PRECISION NOT NULL,"longitude" DOUBLE PRECISION NOT NULL);
+create table "tanbo" ("id" BIGSERIAL NOT NULL PRIMARY KEY,"riceType" INTEGER,"phase" INTEGER NOT NULL,"doneDate" VARCHAR(254) NOT NULL,"latitude" DOUBLE PRECISION NOT NULL,"longitude" DOUBLE PRECISION NOT NULL);
 
 # --- !Downs
 
